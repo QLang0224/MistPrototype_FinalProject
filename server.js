5a884d7c328f92fd26fd2cee9f49230baa8a7a31
ivanc1218 <ivan.contreras@ucdenver.edu>
2024-05-01 15:22:37 -0600

    Retry

--- server.js
+++ server.js
@@ -8,29 +8,6 @@ var cors = require('cors');
 var User = require('./Users');
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://weatherwebapiproject:Weather2024@weather.ank6g9x.mongodb.net/";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MistDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
