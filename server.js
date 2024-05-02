7aea9e6092c60fe9c3cb26437e8c4a5409eb39d8
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-05-02 12:45:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(passport.initialize());
 
 // Set Access-Control-Allow-Origin header globally
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://mist-710j.onrender.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
