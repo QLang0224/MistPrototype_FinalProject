3d63e7c1de61d85c7ee8253a29421cb9a811d91e
ivanc1218 <ivan.contreras@ucdenver.edu>
2024-05-02 15:18:06 -0600

    Added mongoDB integration

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ var User = require('./Users');
 const mongoose = require('mongoose');
 // const fetch = require('node-fetch');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://weatherwebapiproject:Weather2024@weather.ank6g9x.mongodb.net/?retryWrites=true&w=majority&appName=Weather";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
