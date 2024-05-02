2b04a4389666482d8cfa29ed28238052defc2bf9
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-05-02 12:51:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -15,12 +15,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));  
 app.use(passport.initialize());
 
-// Set Access-Control-Allow-Origin header globally
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://mist-710j.onrender.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+    origin: 'https://mist-710j.onrender.com'
+}));
 
 var router = express.Router();
 
