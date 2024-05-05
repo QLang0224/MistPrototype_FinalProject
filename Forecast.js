630cad4e53237b4cccf98a06a19553836d9dc2f7
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-05-04 19:56:08 -0600

    Update Forecast.js

--- Forecast.js
+++ Forecast.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.DB);
 // forecast schema
 const ForecastSchema = new mongoose.Schema({
     timeOfDay: { type: String, required: true, index: true },
-    temperatureFarenheit: { type: Number, min: [-100, 'Must be greater than 1899'], max: [150, 'Must be less than 2100']},
+    temperatureFarenheit: { type: Number, min: [-100, 'Must be greater than -100'], max: [150, 'Must be less than 150']},
     conditions: { type: String, required: true, index: true },
     windSpeed: { type: String, required: true },
     precipitationChance: { type: Number },
